d1e9bb3e4f78074311f316de1956e99aeef6e835
Araya <arayawngwn@gmail.com>
2025-07-06 23:58:08 +0200

    update base

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/glitch-armour/', // <-- your repo name, with slashes
   plugins: [react()],
 })
-
-
